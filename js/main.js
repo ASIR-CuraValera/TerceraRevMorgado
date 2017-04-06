ae242f7b23e56edb6748d4073b8557556958da28
Ikillnukes <ikillnukes1@gmail.com>
2017-04-06 18:14:47 +0200

    Finish!

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-var tog = false;
+var tog = false,
+    active = "inicio";
 
-$(document).ready(function() {
-    //Cargas dinamicas, con asignacion de propiedades
+//Lo único que faltaria seria añadir el contenido, hacer que el boton de cerrar del menu funcione, no que sea al clicar el elemento entero y alguna cosilla con las widths en las animaciones
+
+$(document).ready(function() {    
+    $('[data-menu-section="'+active+'"]').addClass("active");
     
+    //Cargas dinamicas, con asignacion de propiedades
     cargar("header", "web/partials/header.html", function() {
         cargar(".menu", "web/partials/menu/menu.html", function() {
             $(".menu nav").addClass("menu-escritorio");
         });
-        //$(".menu-desp").css("border", "1px solid");
         cargar(".menu-desp", "web/partials/menu/menu.html", function() {
             $(".menu-desp nav").addClass("menu-peque");
         });
-        if(!tog)
-        {
-            $(".menu-desp").css("display", "none");
-            $(".contenido").css("width", "100%")
-        }
-        $(".boton-menu").on('click', menu_toggle);
-        $(".menu-peque .active").on("click", menu_toggle); 
+        $(".boton-menu").on('click', function() {menu_toggle(null); });
+        $(".menu-peque .active").on("click", function() {menu_toggle(null); }); 
     });
     cargar("main", "web/partials/main.html", function() {
         //Cargamos el contenido principal, en este caso, el video
@@ -26,14 +24,36 @@ $(document).ready(function() {
     cargar("footer", "web/partials/footer.html", null);
     
     //Asignación de propiedades, pero esta vez, usando el método de los elementos futuros
+    $("header").on("click", ".menu li:not(.active)", function() {
+        menu_carga(false, $(this).children(0), $(this));   
+    });
+    $("main").on("click", ".menu-desp li:not(.active)", function() {
+        menu_carga(true, $(this).children(0), $(this));   
+    });
     
-    $("header").on("click", ".menu a", menu_carga);
-    $("main").on("click", ".menu-desp a", menu_carga);
+    $("header").on("click", ".menu li:not(.active) a", function() {
+        menu_carga(false, $(this), $(this).parent());
+        return false;
+    });
+    $("main").on("click", ".menu-desp li:not(.active) a", function() {
+        menu_carga(true, $(this), $(this).parent());
+        return false;
+    });
 });
 
-function menu_carga() {
-    cargar(".contenido", "web/html/"+$(this).attr("href"), null);
-    return false;
+function menu_carga(mn, link, parent) {
+    var ruta = $(link).attr("href"),
+        new_act = $(parent).data("menu-section");
+    if(mn) {
+        menu_toggle(function() {
+            cargar(".contenido", "web/html/"+ruta, null);
+        });
+    } else {
+        cargar(".contenido", "web/html/"+ruta, null);
+    }
+    $('[data-menu-section="'+new_act+'"]').addClass("active");
+    $('[data-menu-section="'+active+'"]').removeClass("active");
+    active = new_act;
 }
 
 function cargar(el, ruta, fn) {
@@ -46,19 +66,19 @@ function cargar(el, ruta, fn) {
     });
 }
 
-function menu_toggle() {
+function menu_toggle(fn) {
    if(tog) {
-        $(".menu-desp").animate({width: '0'}, 900, function() {
+        $(".menu-desp").animate({width: '0'}, 1000, function() {
             $(this).hide(); //.effect('slide', { direction: 'left', mode: 'hide' }, 500);
         });
-        $(".contenido").animate({width: '100%'}, 1100);
+        $(".contenido").animate({width: '100%'}, 1000, fn);
         tog = false;
     }
     else {
         $(".menu-desp").show();
-        $(".menu-desp").animate({width: '33.333%'}, 1100);
+        $(".menu-desp").animate({width: '33.333%'}, 1000);
         $(".contenido").css("float", "right");
-        $(".contenido").animate({width: '66%'}, 900);
+        $(".contenido").animate({width: '66%'}, 1000, fn);
         tog = true;
     } 
 }
